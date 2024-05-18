941ce0697a7be3546b8af70c95535b0d5ec0a737
IcySoftware415 <isaacgpadilla07@gmail.com>
2024-05-17 22:42:12 -0600

    + sign in screen initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(20, 50, 20, 100),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2),
+            ),
+            margin: const EdgeInsets.all(40),
+            child: buildSignInForm(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSignInForm() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch, // Use stretch for horizontal alignment
+      children: [
+        buildHeader(),
+        const SizedBox(height: 30),
+        buildTextField('Username'),
+        const SizedBox(height: 30),
+        buildTextField('Password'),
+        const SizedBox(height: 30),
+        buildSignInButton(),
+      ],
+    );
+  }
+
+  Widget buildHeader() {
+    return const Column(
+      children: [
+        Text(
+          'Sign in',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Use your Google Account',
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Widget buildTextField(String hintText) {
+    return TextField(
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        hintText: hintText,
+      ),
+    );
+  }
+
+  Widget buildSignInButton() {
+    return OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      child: const Text('Sign In'),
+    );
+  }
+}
