bb37db17b52f9449b6b020e8bb9167617cccd4f1
IcySoftware415 <isaacgpadilla07@gmail.com>
2024-05-22 15:10:42 -0600

    no changes

--- lib/login/login_main.dart
+++ lib/login/login_main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class LoginMain extends StatelessWidget{
   const LoginMain({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return Center(
